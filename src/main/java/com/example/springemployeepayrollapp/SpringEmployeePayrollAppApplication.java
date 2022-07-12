778d8357a940a2cad77ae547956a3ca6ba157d6e
Ansh4291 <anshulrathore1995@gmail.com>
2022-07-12 20:19:19 +0530

    Added:Welcome message Added in Employee Payroll App

---
+++ src/main/java/com/example/springemployeepayrollapp/SpringEmployeePayrollAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.springemployeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeePayrollAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEmployeePayrollAppApplication.class, args);
+        System.out.println("Hello & Welcome to the employee payroll App");
+    }
+
+}
